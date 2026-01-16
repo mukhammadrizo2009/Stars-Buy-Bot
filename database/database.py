d59131dedf43a99526fb63d123fa6146ace0a32f
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-01-16 14:30:27 +0500

    Database

---
+++ database/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from .config import config
+
+url = URL.create(
+    drivername="postgresql+psycopg2",
+    host=config.DB_HOST,
+    port=config.DB_PORT,
+    username=config.DB_USER,
+    password=config.DB_PASSWORD,
+    database=config.DB_NAME 
+)
+
+engine = create_engine(url)
+Base = declarative_base()
+LocalSession = sessionmaker(bind=engine , autoflush=True , autocommit=False)
---
+++ database/dependencies.py
@@ -0,0 +1,8 @@
+from .database import LocalSession
+
+def get_db():
+    db = LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
