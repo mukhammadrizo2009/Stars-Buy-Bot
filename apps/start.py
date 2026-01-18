191ce246150834fdb92f7ec1c47c70b4d9d765b2
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-01-18 19:17:59 +0500

    Start

--- apps/start.py
+++ apps/start.py
@@ -1,4 +1,4 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram import (Update, ReplyKeyboardMarkup, KeyboardButton)
 from telegram.ext import CallbackContext
 
 def start(update: Update, context: CallbackContext):
